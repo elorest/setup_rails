3cf93372c48fd6b79ae393912f9d28fe75895461
Isaac Sloan <isaac@isaacsloan.com>
2013-04-07 17:31:32 -0600

    Update setup_rails.sh
    install imagemagick by default

--- setup_rails.sh
+++ setup_rails.sh
@@ -12,6 +12,7 @@ gem install bundler
 rbenv rehash
 echo 'gem: --no-rdoc --no-ri' >> ~/.gemrc
 brew install sqlite3
+brew install imagemagick
 brew install mysql
 unset TMPDIR
 mysql_install_db --verbose --user=`whoami` --basedir="$(brew --prefix mysql)" --datadir=/usr/local/var/mysql --tmpdir=/tmp
